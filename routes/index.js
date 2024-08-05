d8a928e777feb6c4d819862ed687e9b046dfddfe
umbrella-hanseo-jo <0214gxqs@gmail.com>
2024-08-05 10:17:48 +0900

    최초 로컬저장소 커밋

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'bird_hans_' });
+});
+
+module.exports = router;
